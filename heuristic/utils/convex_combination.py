31e82b46647c2c46565e4dd0ca3cafa4408a2932
Niels Wouda <nielswouda@gmail.com>
2019-05-22 14:04:08 +0200

    Better explanation

--- heuristic/utils/convex_combination.py
+++ heuristic/utils/convex_combination.py
@@ -2,5 +2,9 @@ from heuristic.Configuration import Configuration
 
 
 def convex_combination(first: float, second: float) -> float:
+    """
+    Returns a convex combination of two weights, using the ``OPERATOR_DECAY``
+    parameter to combine them.
+    """
     return sum([Configuration.OPERATOR_DECAY * first,
                 (1 - Configuration.OPERATOR_DECAY) * second])

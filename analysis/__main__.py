ca22ad5fd0507d752fd4548b52a703977e77e124
Niels Wouda <nielswouda@gmail.com>
2020-05-05 12:04:36 +0200

    Improvements to analysis tool

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 from pathlib import Path
 
 import numpy as np
@@ -7,35 +7,57 @@ import pandas as pd
 from heuristic.classes import Problem, Solution
 
 pd.set_option('display.max_rows', None)  # display all rows.
+pd.set_option('display.float_format', '{:.2f}'.format)  # two decimals.
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="analysis",
+                                     description="Analyse experiment results.")
+
+    parser.add_argument("method",
+                        type=str,
+                        help="Solution method to analyse."
+                             " One of {ilp, heuristic}.")
+
+    parser.add_argument("experiment", type=int, help="experiment number")
+
+    return parser, parser.parse_args()
 
 
 def main():
-    experiment = int(sys.argv[1])
+    parser, args = parse_args()
     instances = np.arange(1, 101)
 
-    cache = Path(f"analysis/cache/{experiment}.csv")
+    cache = Path(f"analysis/cache/{args.experiment}-{args.method}.csv")
 
     if cache.exists():
-        print(f"{sys.argv[0]}: re-using {cache}.")
+        print(f"{parser.prog}: re-using {cache}.")
         data = pd.read_csv(cache)
     else:
         objectives = []
 
         for instance in instances:  # gather stats for each instance in exp.
-            Problem.from_instance(experiment, instance)
+            Problem.from_instance(args.experiment, instance)
 
-            location = f"experiments/{experiment}/{instance}-heuristic.json"
+            location = Path(f"experiments/{args.experiment}/"
+                            f"{instance}-{args.method}.json")
+
+            if not location.exists():
+                print(f"{parser.prog}: {location} does not exist; skipping.")
+                continue
+
             solution = Solution.from_file(location)
+            objectives.append((instance, -solution.objective()))
 
-            objectives.append(-solution.objective())
+        data = pd.DataFrame.from_records(objectives,
+                                         columns=("instance", "objective"))
 
-        data = pd.DataFrame(objectives, instances, columns=["objective"])
+    data.set_index("instance", inplace=True)
 
     print(data, '\n')
-    print("Aggregates:")
-    print(data.aggregate("mean"))
+    print("Aggregates:", data.aggregate("mean"), sep="\n")
 
-    data.to_csv(f"analysis/cache/{experiment}.csv", index=False)
+    data.to_csv(cache)
 
 
 if __name__ == "__main__":

c65a35a57e67f62e16c9f964773e577525066242
Niels Wouda <nielswouda@gmail.com>
2021-02-21 22:34:49 +0100

    Allow excluding operators to test operator quality

--- heuristic/__main__.py
+++ heuristic/__main__.py
@@ -9,37 +9,57 @@ from heuristic.functions import initial_solution
 from .destroy_operators import DESTROY_OPERATORS
 from .local_search import reinsert_learner
 from .repair_operators import REPAIR_OPERATORS
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="heuristic")
+
+    parser.add_argument("experiment", type=str)
+    parser.add_argument("instance", type=int)
+    parser.add_argument("--exclude", type=str, default=None)
+
+    args = parser.parse_args()
+    args.experiment = "tuning" if args.experiment == "tuning" else int(args.experiment)
+
+    return args
 
 
 def main():
-    experiment = "tuning" if sys.argv[1] == "tuning" else int(sys.argv[1])
-    instance = int(sys.argv[2])
+    args = parse_args()
 
-    Problem.from_instance(experiment, instance)
+    Problem.from_instance(args.experiment, args.instance)
 
-    if experiment == "tuning":
-        generator = rnd.default_rng(instance)
+    if args.experiment == "tuning":
+        generator = rnd.default_rng(args.instance)
     else:
         # E.g. for exp 72 and inst. 1, this becomes 7201. This way, even for
         # inst. 100, there will never be overlap between random number streams
         # across experiments.
-        generator = rnd.default_rng(100 * experiment + instance)
+        generator = rnd.default_rng(100 * args.experiment + args.instance)
 
     alns = ALNS(generator)  # noqa
 
     for operator in DESTROY_OPERATORS:
+        if args.exclude == operator.__name__:
+            continue
+
         alns.add_destroy_operator(operator)
 
     for operator in REPAIR_OPERATORS:
+        if args.exclude == operator.__name__:
+            continue
+
         alns.add_repair_operator(operator)
 
-    alns.on_best(reinsert_learner)
+    if args.exclude == "reinsert_learner":
+        alns.on_best(reinsert_learner)
 
     init = initial_solution()
     criterion = get_criterion(init.objective())
     result = alns.iterate(init, WEIGHTS, DECAY, criterion, ITERATIONS)
 
-    location = f"experiments/{experiment}/{instance}-heuristic.json"
+    location = f"experiments/{args.experiment}/{args.instance}-heuristic.json"
     result.best_state.to_file(location)  # noqa
 
 

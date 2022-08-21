2b999e4ca1b6e606b8dd04e56ad552e5eb752135
Niels Wouda <nielswouda@gmail.com>
2022-08-21 14:56:00 +0200

    Remove most slack classroom

--- src/destroy_operators/__init__.py
+++ src/destroy_operators/__init__.py
@@ -1,11 +1,9 @@
-from .most_slack_classrooms import most_slack_classrooms
 from .random_activities import random_activities
 from .random_learners import random_learners
 from .smallest_activities import smallest_activities
 from .regret_learners import regret_learners
 
 DESTROY_OPERATORS = [
-    most_slack_classrooms,
     random_activities,
     random_learners,
     regret_learners,
